d5341e78cf06917eca30cabccaeb58c15d81c0bb
a76yyyy <q981331502@163.com>
2022-06-12 09:32:04 +0800

    Add command for feed

---
+++ cmd/feed/command/feed.go
@@ -0,0 +1,73 @@
+package command
+
+import (
+	"context"
+	"sort"
+	"time"
+
+	"github.com/a76yyyy/tiktok/cmd/user/command"
+	"github.com/a76yyyy/tiktok/pkg/errno"
+
+	"github.com/a76yyyy/tiktok/kitex_gen/feed"
+	"github.com/a76yyyy/tiktok/kitex_gen/user"
+
+	"github.com/a76yyyy/tiktok/dal/db"
+)
+
+type GetUserFeedService struct {
+	ctx context.Context
+}
+
+// NewGetUserFeedService new GetUserFeedService
+func NewGetUserFeedService(ctx context.Context) *GetUserFeedService {
+	return &GetUserFeedService{ctx: ctx}
+}
+
+// GetUserFeed get feed info.
+func (s *GetUserFeedService) GetUserFeed(req *feed.DouyinFeedRequest, uid int) (vis []*feed.Video, nextTime int64, err error) {
+
+	videos, err := db.MGetVideos(s.ctx, *req.LatestTime)
+	if err != nil {
+		return vis, nextTime, err
+	}
+	if len(videos) == 0 {
+		return vis, nextTime, errno.ErrVideoNotFound
+	}
+
+	if len(videos) > 0 {
+		sort.Slice(videos, func(i, j int) bool {
+			return videos[i].UpdatedAt.UnixMilli() > videos[j].UpdatedAt.UnixMilli()
+		})
+		nextTime = videos[len(videos)-1].UpdatedAt.UnixMilli()
+	} else {
+		nextTime = time.Now().UnixMilli()
+	}
+	for _, v := range videos {
+		user, err := command.NewMGetUserService(s.ctx).MGetUser(&user.DouyinUserRequest{UserId: int64(v.AuthorID)})
+		if err != nil {
+			return vis, nextTime, err
+		}
+		flag := false
+		if uid != 0 {
+			if result, err := db.GetFavoriteVideo(s.ctx, uid, int(v.ID)); err != nil {
+				return vis, nextTime, err
+			} else if result.VideoID > 0 {
+				flag = true
+			} else {
+				flag = false
+			}
+
+		}
+		vis = append(vis, &feed.Video{
+			Id:            int64(v.ID),
+			Author:        user,
+			PlayUrl:       v.PlayUrl,
+			CoverUrl:      v.CoverUrl,
+			FavoriteCount: int64(v.FavoriteCount),
+			CommentCount:  int64(v.CommentCount),
+			IsFavorite:    flag, // TODO 判断这个视频是否自己喜欢
+		})
+	}
+
+	return vis, nextTime, nil
+}
--- cmd/feed/handler.go
+++ cmd/feed/handler.go
@@ -2,13 +2,9 @@ package main
 
 import (
 	"context"
-	"sort"
-	"time"
 
-	"github.com/a76yyyy/tiktok/cmd/user/command"
-	"github.com/a76yyyy/tiktok/dal/db"
+	"github.com/a76yyyy/tiktok/cmd/feed/command"
 	"github.com/a76yyyy/tiktok/kitex_gen/feed"
-	"github.com/a76yyyy/tiktok/kitex_gen/user"
 	"github.com/a76yyyy/tiktok/pack"
 	"github.com/a76yyyy/tiktok/pkg/errno"
 )
@@ -29,51 +25,10 @@ func (s *FeedSrvImpl) GetUserFeed(ctx context.Context, req *feed.DouyinFeedReque
 		}
 	}
 
-	videos, err := db.MGetVideos(ctx, *req.LatestTime)
+	vis, nextTime, err := command.NewGetUserFeedService(ctx).GetUserFeed(req, uid)
 	if err != nil {
 		resp = pack.BuildVideoResp(err)
 		return resp, nil
-	}
-	if len(videos) == 0 {
-		resp = pack.BuildVideoResp(errno.ErrVideoNotFound)
-		return resp, nil
-	}
-	var vis []*feed.Video
-	var nextTime int64
-	if len(videos) > 0 {
-		sort.Slice(videos, func(i, j int) bool {
-			return videos[i].UpdatedAt.UnixMilli() > videos[j].UpdatedAt.UnixMilli()
-		})
-		nextTime = videos[len(videos)-1].UpdatedAt.UnixMilli()
-	} else {
-		nextTime = time.Now().UnixMilli()
-	}
-	for _, v := range videos {
-		user, err := command.NewMGetUserService(ctx).MGetUser(&user.DouyinUserRequest{UserId: int64(v.AuthorID)})
-		if err != nil {
-			return pack.BuildVideoResp(err), nil
-		}
-		flag := false
-		if uid != 0 {
-			if result, err := db.GetFavoriteVideo(ctx, uid, int(v.ID)); err != nil {
-				resp = pack.BuildVideoResp(err)
-				return resp, nil
-			} else if result.VideoID > 0 {
-				flag = true
-			} else {
-				flag = false
-			}
-
-		}
-		vis = append(vis, &feed.Video{
-			Id:            int64(v.ID),
-			Author:        user,
-			PlayUrl:       v.PlayUrl,
-			CoverUrl:      v.CoverUrl,
-			FavoriteCount: int64(v.FavoriteCount),
-			CommentCount:  int64(v.CommentCount),
-			IsFavorite:    flag, // TODO 判断这个视频是否自己喜欢
-		})
 	}
 
 	resp = pack.BuildVideoResp(errno.Success)
